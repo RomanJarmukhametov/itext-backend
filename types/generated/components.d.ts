1328516e3bd29b52e5a987cc552fa9b0972a2ff4
Roman Jarmukhametov <roman.jarmukhametov@gmail.com>
2024-11-12 20:45:11 +0500

    feat: add Features layout component

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,19 @@
 import type { Schema, Struct } from '@strapi/strapi';
 
+export interface ComponentsCard extends Struct.ComponentSchema {
+  collectionName: 'components_components_cards';
+  info: {
+    displayName: 'Card';
+  };
+  attributes: {
+    description: Schema.Attribute.Text;
+    heading: Schema.Attribute.String;
+    icon: Schema.Attribute.Enumeration<
+      ['Professional', 'Specialized', 'Automation']
+    >;
+  };
+}
+
 export interface ComponentsNavigationItem extends Struct.ComponentSchema {
   collectionName: 'components_components_navigation_items';
   info: {
@@ -8,6 +22,19 @@ export interface ComponentsNavigationItem extends Struct.ComponentSchema {
   attributes: {
     label: Schema.Attribute.String;
     link: Schema.Attribute.String;
+  };
+}
+
+export interface LayoutFeatures extends Struct.ComponentSchema {
+  collectionName: 'components_layout_features';
+  info: {
+    displayName: 'Features';
+  };
+  attributes: {
+    card: Schema.Attribute.Component<'components.card', true>;
+    description: Schema.Attribute.Text;
+    heading: Schema.Attribute.String;
+    tagline: Schema.Attribute.String;
   };
 }
 
@@ -31,7 +58,9 @@ export interface LayoutHero extends Struct.ComponentSchema {
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
+      'components.card': ComponentsCard;
       'components.navigation-item': ComponentsNavigationItem;
+      'layout.features': LayoutFeatures;
       'layout.hero': LayoutHero;
     }
   }
